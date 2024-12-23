74b983aa7eff756bfe8aea22f997edf4afdea8f1
3bdyem <moustafa3bdayem@gmail.com>
2024-12-23 07:40:15 +0200

    Add files via upload

---
+++ annotaion_and_docstring.py
@@ -0,0 +1,15 @@
+def add_numbers(a: int, b: int) -> int:
+    """
+    Adds two numbers and returns the result.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    int: The sum of the two numbers.
+    """
+    return a + b
+
+result = add_numbers(5, 10)
+print(result)
